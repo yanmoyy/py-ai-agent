36bc19ed7f3f665bfeaf925ccc8d30726e2581e2
yanmo <67538999+yanmoyy@users.noreply.github.com>
2025-06-14 20:35:29 +0900

    Add get_files_info function

---
+++ functions/get_files_info.py
@@ -0,0 +1,40 @@
+import os
+
+
+class FileInfo:
+    def __init__(self, name, size, id_dir=False):
+        self.name = name
+        self.size = size
+        self.id_dir = id_dir
+
+    def __str__(self):
+        return f"{self.name}: file_size={self.size} bytes, is_dir={self.id_dir}"
+
+
+def get_files_info(working_directory, directory=None):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = abs_working_dir
+    if directory:
+        target_dir = os.path.abspath(
+            os.path.join(working_directory, directory),
+        )
+
+    if not target_dir.startswith(abs_working_dir):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
+
+    try:
+        file_info = []
+        for filename in os.listdir(target_dir):
+            file_path = os.path.join(target_dir, filename)
+            if os.path.isdir(file_path):
+                file_info.append(FileInfo(filename, os.path.getsize(file_path), True))
+            else:
+                file_info.append(FileInfo(filename, os.path.getsize(file_path)))
+
+        str_file_info = "\n".join(["- " + str(info) for info in file_info])
+        return str_file_info
+    except Exception as e:
+        return f"Error listing files: {e}"
---
+++ tests.py
@@ -0,0 +1,27 @@
+from functions.get_files_info import get_files_info
+
+
+def test():
+    result = get_files_info("calculator", ".")
+    print("Result for current directory:")
+    print(result)
+    print("")
+
+    result = get_files_info("calculator", "pkg")
+    print("Result for 'pkg' directory:")
+    print(result)
+    print("")
+
+    result = get_files_info("calculator", "/bin")
+    print("Result for '/bin' directory:")
+    print(result)
+    print("")
+
+    result = get_files_info("calculator", "../")
+    print("Result for '../' directory:")
+    print(result)
+    print("")
+
+
+if __name__ == "__main__":
+    test()
